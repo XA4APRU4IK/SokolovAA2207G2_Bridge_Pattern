980c1b55b1ffb3795e101270f523af1a2df741ae
XA4APRU4IK <cokolov123123@mail.ru>
2024-10-28 11:47:29 +0700

    Add files via upload

---
+++ Bridge Pattern/Bridge pattern/Program.cs
@@ -0,0 +1,62 @@
+﻿public interface INotification
+{
+    void Send(string message);
+}
+public class EmailNotification : INotification
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Отправка Email: {message}");
+    }
+}
+public class SmsNotification : INotification
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Отправка SMS: {message}");
+    }
+}
+public abstract class NotificationSender
+{
+    protected INotification notification;
+
+    protected NotificationSender(INotification notification)
+    {
+        this.notification = notification;
+    }
+    public abstract void Notify(string message);
+}
+public class TextNotificationSender : NotificationSender
+{
+    public TextNotificationSender(INotification notification) : base(notification) { }
+
+    public override void Notify(string message)
+    {
+        notification.Send(message);
+    }
+}
+
+public class HtmlNotificationSender : NotificationSender
+{
+    public HtmlNotificationSender(INotification notification) : base(notification) { }
+
+    public override void Notify(string message)
+    {
+        string htmlMessage = $"<html><body>{message}</body></html>";
+        notification.Send(htmlMessage);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        INotification email = new EmailNotification();
+        INotification sms = new SmsNotification();
+
+        NotificationSender textEmailSender = new TextNotificationSender(email);
+        NotificationSender htmlSmsSender = new HtmlNotificationSender(sms);
+
+        textEmailSender.Notify("Это текстовое уведомление по Email.");
+        htmlSmsSender.Notify("Это HTML-уведомление по SMS.");
+    }
+}
